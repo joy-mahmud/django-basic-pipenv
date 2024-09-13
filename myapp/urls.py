a0016d9ae3e1b24751f4f79a4a04a5469175408c
joy-mahmud <joymahmudiucse@gmail.com>
2024-09-13 23:52:35 +0600

    template inheritance

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[path('home/',views.myappHome)]
+urlpatterns =[
+    path('home/',views.myappHome),
+    path('about/',views.about),
+    path('profile/',views.profile),
+    ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def myappHome(request):
-    return render(request,'index.html')
-
+def myappHome(req):
+    return render(req,'index.html',{"title":'home','content':'This is home content'})
+def about(request):
+    page={
+        'title':'about',
+        'content':'This is about page content'
+    }
+    return render(request,'about.html',page)
+def profile(request):
+     social =['facebook:me.facebook.com','github:me.github.com','instagram:me.instagram.com']
+     return render(request,'profile.html',{'title':'profile','social':social})
 # Create your views here.
