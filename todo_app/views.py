ea92e5f046bc722e23ecdbc7ca1bab25bc498faf
joy-mahmud <joymahmudiucse@gmail.com>
2024-09-17 23:17:47 +0600

    data fething from database and showing the tasks

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Task
+def TaskView(request):
+    tasks = Task.objects.all()
+    return render(request,'task_list.html',{"tasks":tasks})
 # Create your views here.
