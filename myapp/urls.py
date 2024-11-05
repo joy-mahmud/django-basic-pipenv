ac87b81b0cb8c80bdb8d472534a8395242d96a46
joy-mahmud <joymahmudiucse@gmail.com>
2024-11-05 21:21:13 +0600

    data insertion into database by creating model

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns =[
     path('home/',views.myappHome),
     path('about/',views.about),
     path('profile/',views.profile),
-    
+    path('addBook/',views.insert_data),
+    path('allbooks/',views.display_books)
     ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Book
 def myappHome(req):
     return render(req,'index.html',{"title":'home','content':'This is home content'})
 def about(request):
@@ -11,4 +12,20 @@ def about(request):
 def profile(request):
      social =['facebook:me.facebook.com','github:me.github.com','instagram:me.instagram.com']
      return render(request,'profile.html',{'title':'profile','social':social})
-# Create your views here.
+ 
+#inserting data to database:
+def insert_data(request):
+    book = Book(
+        title="double standard",
+        author="shakti",
+        publish_date="2005-03-11",
+        isbn_number="849474847484789"
+    )
+    
+    book.save()
+    return HttpResponse("Data inserted successfully")
+
+def display_books(request):
+    books = Book.objects.all().values()
+    print(books)
+    return render(request,"allBooks.html",context={"allBooks":books})
