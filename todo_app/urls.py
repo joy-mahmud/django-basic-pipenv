79be43f092b4a84c9fcf4b3ff9c7037c8fa6b66e
joy-mahmud <joymahmudiucse@gmail.com>
2024-11-16 23:20:54 +0600

    taskudate

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views 
 urlpatterns =[
-    path('',views.TaskView),
+    path('',views.TaskView,name='task_list'),
     path('<int:id>/',views.task_details,name="task_details"),
+    path('update/<int:id>',views.update_task,name="update_task")
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 def TaskView(request):
     tasks = Task.objects.all()
     return render(request,'task_list.html',{"tasks":tasks})
@@ -7,3 +9,19 @@ def TaskView(request):
 def task_details(req,id):
     task = Task.objects.get(pk=id)
     return render(req,'task_details.html',{"task":task})
+
+def update_task(request,id):
+    try:
+        task=Task.objects.get(id=id)
+        if request.method == "POST":
+            task_form=TaskForm(request.POST,instance=task)
+            if task_form.is_valid():
+                task_form.save()
+                return redirect('task_list')
+            else:
+                task_form=TaskForm(request.POST,instance=task)
+                return render(request,'task_update.html',{'form':task_form}) 
+        task_form =TaskForm(instance=task)
+        return render(request,'task_update.html',{'form':task_form})   
+    except Task.DoesNotExist:
+        return HttpResponse('Task does not exist')
