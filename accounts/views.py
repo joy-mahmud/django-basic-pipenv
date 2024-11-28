5d3f80a9ff641326f10f3346f5b0a045e04ddf1d
joy-mahmud <joymahmudiucse@gmail.com>
2024-11-28 21:02:07 +0600

    custom register form

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,27 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login
 # Create your views here.
+# from django.contrib.auth.models import User
+# from django import forms
+# class CustomUserCreationForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
 
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
+#         help_texts = {
+#             'username': None,  # Remove the "150 characters or fewer" help text
+#             'password1': None,  # Remove default password help text
+#             'password2': None,
+#         }
+        
+#         def __init__(self, *args, **kwargs):
+#             super().__init__(*args, **kwargs)
+#             self.fields['password'].help_text = None
+#             self.fields['password'].help_text = None
+        
+        
+        
 def home(request):
     return render(request,'accounts/home.html')
 
@@ -18,15 +38,15 @@ def register(request):
         context={
             "form":form
         }
-        return render(request,'accounts/register.html',context)
+        return render(request,'accounts/register.html')
     
 def signIn(request):
     if request.method=="POST":
         form=AuthenticationForm(data=request.POST)
         if form.is_valid():
-           user=form.get_user()
-        login(request,user)
-        return redirect('account_home') 
+            user=form.get_user()
+            login(request,user)
+            return redirect('account_home') 
         
         
     form=AuthenticationForm()
