24413bacc1ce3c0c2ae97280c9b8e19fa3406cf7
joy-mahmud <joymahmudiucse@gmail.com>
2024-11-30 23:54:43 +0600

    person app changed

--- person/urls.py
+++ person/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home),
-    path('addPerson/',views.add_person)
+    path('addPerson/',views.add_person),
+    path('allPersons/',views.view_all_person),
+    path('personDetails/',views.person_details)
 ]
--- person/views.py
+++ person/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 from .models import personCollection,printCollection
 
 # Create your views here.
@@ -22,5 +24,19 @@ def add_person(request):
     ]
     personCollection.insert_many(record)
    
-    
     return HttpResponse('person record added successfully')
+def view_all_person(request):
+    persons=personCollection.find()
+    person_json=[]
+    for person in persons:
+        person['_id']=str(person['_id'])
+        person_json.append(person)
+    return JsonResponse(person_json,safe=False)
+
+def person_details(request):
+    id="67497dcefb9111c61bf0cc57"
+    person= personCollection.find_one({'_id':ObjectId(id)})
+    print(person['name'])
+    person['_id']=str(person['_id'])
+    person_json=person
+    return JsonResponse(person_json,safe=False)
